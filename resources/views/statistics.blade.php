44312650cab7df08005c94ca60d14fe5d587a2c5
galatsiuk.taras <galatsiuk.taras@gmail.com>
2020-02-16 17:58:00 +0200

    changed title in statistics

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card" >
-                    <div class="card-header">Manage Bookings</div>
+                    <div class="card-header">Statistics</div>
 
                     <div class="card-body tabs-body">
                         @if (session('status'))
